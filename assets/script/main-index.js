e4dc3181a27517b725081e9ae690ac5cca52bde1
IvanCardoso91 <ivancardoso1991@gmail.com>
2020-12-20 01:46:38 -0300

    SENPAI-kuuuunnn

--- assets/script/main-index.js
+++ assets/script/main-index.js
@@ -10,15 +10,29 @@ function myFunction(option) {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.menu-options-btn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+window.onclick = ({ target }) => {
+    if (!target.matches('.menu-options-btn')) {
+        var dropdown = document.querySelectorAll(".dropdown-content")
+
+        dropdown.forEach(item => {
+            if (item.classList.contains('show')) {
+                item.classList.remove('show');
             }
-        }
+        })
+    }
+}
+
+const accordionsInfo = document.querySelectorAll('.info');
+
+function openAccordion({ target: { nextElementSibling } }) {
+    const info = nextElementSibling;
+    info.style.display = info.style.display === 'none' ? '' : 'none';
+}
+
+window.onclick = ({ target }) => {
+    if (!target.matches('.accordion')) {
+        accordionsInfo.forEach(item => {
+            if (item.style.display === '') item.style.display = 'none';
+        });
     }
 }
