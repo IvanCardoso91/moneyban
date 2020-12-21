9ad432b52cd1bef45e18675093198d19d97bfd26
IvanCardoso91 <ivancardoso1991@gmail.com>
2020-12-21 18:35:22 -0300

    alteracoes posicionamento

--- assets/script/main-index.js
+++ assets/script/main-index.js
@@ -6,28 +6,11 @@ function myFunction(option) {
         document.getElementById("enterpriseDropdown").classList.toggle("show");
         document.getElementById("myDropdown").classList.remove("show");
     }
-
 }
 
 function menuMobile() {
     document.getElementById("dropdownMenuMobile").classList.toggle("show");
 }
-
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//     console.log('oi')
-//     if (!event.target.matches('.menu-options-btn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
-
 
 const accordionsInfo = document.querySelectorAll('.info');
 
